de4057758f799fefc01e8948bcaaddf5e7045094
Vishal <vishalchoudhary34510@gmail.com>
2023-03-20 10:43:13 +0530

    best

--- Routes/email.js
+++ Routes/email.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: "013vishalchoudhary@gmail.com",
-    pass: "xendfnlcogkrxoif",
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
